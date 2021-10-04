b343f6ae8e5c82a36deacd6a7c2416af2a350c6c
carlaval655 <carla.val655@gmail.com>
2021-10-03 20:52:10 -0400

    Subiendo bot multiusuario

--- src/main/java/bo/edu/ucb/est/Calculadora.java
+++ src/main/java/bo/edu/ucb/est/Calculadora.java
@@ -5,7 +5,9 @@
  */
 package bo.edu.ucb.est;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -17,110 +19,118 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
  * @author Windows
  */
 public class Calculadora extends TelegramLongPollingBot {
-    Suma suma = new Suma();
-    int val = 1; //Valida la primera interaccion con el usuario
-    SendMessage mensajeEnviado = new SendMessage();
-    Pantalla pantalla = new Pantalla();
-    int opcion = 0;
-    public void inicializarBot(Update update){
-        
-    }
+    /**
+     * estado = 0 >>> Debo mostrar menu inicial
+     * estado = 1 >>> Pedir un numero del menu de opciones
+     * estado = 2 >>> Menu sumar y debo pedir primer numero
+     * estado = 3 >>> Menu sumar y pidiendo segundo numero
+     * estado = 4 >>> Menu fibonacci
+     */
+    private Map estadoUsuario = new HashMap();
+    private Map numeroUnoUsuario = new HashMap();
+    private Map numeroDosUsuario = new HashMap();
+
+    
     @Override
     public String getBotToken() {
         return "2023303071:AAGm3zEb-mtOQjw-y2d3Ma07lBPPKiPGIPo";
     }
 
     @Override
-    public void onUpdateReceived(Update update) {
-        System.out.println("Llego mensaje: " + update.toString());
-        if (val==1){
-                mostrarMenuInicio(update);
-                val++;
-            }
+        public void onUpdateReceived(Update update) {
+        Long userId = update.getMessage().getChatId();
+        Integer estado = (Integer) estadoUsuario.get(userId);
+        if (estado == null){
+            estado = 0;
+            estadoUsuario.put(userId,estado);
+        }
+        System.out.println(update.getMessage().toString());
         if(update.hasMessage()) {
-            /*
-            Procedemos a comprobar que mensaje envio el bot para de acuerdo a esto validar la respuesta del usuario
-            */
-            if (mensajeEnviado.getText().equals(pantalla.enviarMenu())){
-                mensajeEnviado.setChatId(update.getMessage().getChatId().toString()); //Define a quien le vamos a enviar el mensaje
-                opcion = pantalla.eleccion(mensajeEnviado, update);
-                switch (opcion){
-                    case 1:{
-                        mensajeEnviado.setText("Introduzca el primer número: ");
-                        try {
-                            execute(mensajeEnviado);
-                        } 
-                        catch (TelegramApiException ex) {
-                            Logger.getLogger(Calculadora.class.getName()).log(Level.SEVERE, null, ex);
-                        }  
-                    }
-                    break;
-                    case 2:{
-                        mensajeEnviado.setText("Funcionalidad no implementada. Intente otro día. ");
-                        try {
-                            execute(mensajeEnviado);
-                            mostrarMenuInicio(update);
-                        } 
-                        catch (TelegramApiException ex) {
-                            Logger.getLogger(Calculadora.class.getName()).log(Level.SEVERE, null, ex);
+            
+            String mensajeAMostrar = null;
+            String menuInicial = "Bienvenido al Bot Calculadora!\nElija una opcion\n1. Sumar dos numeros.\n2. Fibonacci.\n Por favor elija una opcion: ";
+            String primerNumero = "Ingrese el primer numero";
+            String segundoNumero = "Ingrese el segundo numero";
+            String resultadoSuma = "El resultado es: ";
+            String fibonacci = "Fibonacci aun no esta disponible retorne otro dia";
+            String mensajeUsuario = null;
+            switch (estado){
+                case 0: 
+                mostrarMensaje(menuInicial, String.valueOf(userId));
+                estadoUsuario.put(userId,1);
+                break;
+                case 1: 
+                    mensajeUsuario = update.getMessage().getText();
+                    try {
+                        int opcion = Integer.parseInt(mensajeUsuario);
+                        if(opcion==1 || opcion==2){
+                            if (opcion==1){ //Quiere sumar dos numeros
+                                mostrarMensaje(primerNumero, String.valueOf(userId));
+                                estadoUsuario.put(userId,2);
+                            }
+                            else {
+                                mostrarMensaje(fibonacci, String.valueOf(userId));
+                                mostrarMensaje(menuInicial, String.valueOf(userId));
+                                estadoUsuario.put(userId,1);
+                            }
+                        }
+                        else{
+                            mostrarMensaje(menuInicial, String.valueOf(userId));
+                            estadoUsuario.put(userId,1);
                         }
                     }
-                        break;
-                    default:
-                            mostrarMenuInicio(update);
-                            break;
-                }
-            }
-            else{
-                if (mensajeEnviado.getText().equals("Introduzca el primer número: ")){
-                if(suma.validarNumero(update.getMessage().getText())==true){
-                    suma.setNumero1(Integer.parseInt(update.getMessage().getText()));
-                    mensajeEnviado.setText("Introduzca el segundo número: ");
-                    mensajeEnviado.setChatId(update.getMessage().getChatId().toString());
-                    try {
-                        execute(mensajeEnviado);
-                    } 
-                    catch (TelegramApiException ex) {
-                        Logger.getLogger(Calculadora.class.getName()).log(Level.SEVERE, null, ex);
+                    catch(Exception ex){
+                        mostrarMensaje(menuInicial, String.valueOf(userId));
+                        estadoUsuario.put(userId,1);
                     }
-                }
-                else
-                    mostrarMenuInicio(update);
-            }
-                else{
-                    if (mensajeEnviado.getText().equals("Introduzca el segundo número: ")){
-                if(suma.validarNumero(update.getMessage().getText())==true){
-                    suma.setNumero2(Integer.parseInt(update.getMessage().getText()));
-                    mensajeEnviado.setText("La suma es: "+suma.calcularSuma());
-                    mensajeEnviado.setChatId(update.getMessage().getChatId().toString());
-                    try {
-                        execute(mensajeEnviado);
-                        mostrarMenuInicio(update);
-                    } 
-                    catch (TelegramApiException ex) {
-                        Logger.getLogger(Calculadora.class.getName()).log(Level.SEVERE, null, ex);
+                    break;
+                case 2:
+                     mensajeUsuario = update.getMessage().getText();
+                     try {
+                        numeroUnoUsuario.put(userId,Integer.parseInt(mensajeUsuario));
+                        mostrarMensaje(segundoNumero, String.valueOf(userId));
+                        estadoUsuario.put(userId,3);
                     }
-                }
-                else
-                    mostrarMenuInicio(update);
+                    catch(Exception ex){
+                        mostrarMensaje(menuInicial, String.valueOf(userId));
+                        estadoUsuario.put(userId,0);
+                    }
+                     break;
+                case 3:
+                     mensajeUsuario = update.getMessage().getText();
+                     try {
+                        int numeroDos = Integer.parseInt(mensajeUsuario);
+                        int numeroUno = (int) numeroUnoUsuario.get(userId);
+                        mostrarMensaje(resultadoSuma + (numeroUno + numeroDos), String.valueOf(userId));
+                        mostrarMensaje(menuInicial, String.valueOf(userId));
+                        estadoUsuario.put(userId,1);
+                    }
+                    catch(Exception ex){
+                        mostrarMensaje(menuInicial, String.valueOf(userId));
+                        estadoUsuario.put(userId,1);
+                    }
+                     break;
+                default:
+                    mostrarMensaje(menuInicial, String.valueOf(userId));
+                    estadoUsuario.put(userId,1);
+                    break;
             }
-            }
+            
         }
-    }
     }
     @Override
     public String getBotUsername() {
         return "Calculadora_est_bot";
     }
-    
-    public void mostrarMenuInicio(Update update){
-                    mensajeEnviado.setText(pantalla.enviarMenu());
-                    mensajeEnviado.setChatId(update.getMessage().getChatId().toString());
-                try {
-                    execute(mensajeEnviado);
-                } 
-                catch (TelegramApiException ex) {
-                    Logger.getLogger(Calculadora.class.getName()).log(Level.SEVERE, null, ex);
-                }
+    public void mostrarMensaje(String mensaje, String Id){
+        SendMessage message = new SendMessage();
+        message.setChatId(Id);
+        message.setText(mensaje);
+            message.setChatId(Id+"");
+            try {
+                execute(message);
+            } catch (TelegramApiException ex) {
+                
+            }
     }
 }
